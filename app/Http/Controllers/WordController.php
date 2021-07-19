b827091ad378f10bd795d2d300cad4631c9f4bbf
Vladyslav Prokhorenko <random.person.1907@gmail.com>
2021-07-20 02:34:56 +0300

    Exclude learned word from list.

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\WordResource;
 use App\Models\Dictionary;
+use App\Models\User;
 use App\Models\Word;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -14,6 +15,7 @@ class WordController extends Controller
     {
         $words = Word::query()
                     ->where('dictionary_id', $dictionaryId)
+                    ->whereNotIn('id', User::current()->learnedWords()->pluck('id'))
                     ->with('audio')
                     ->paginate();
 
